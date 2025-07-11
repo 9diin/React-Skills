6bc5f6d9cad22e9ad01b6e2f7f18e0b6eb0f753a
9Diin <weartstudio@naver.com>
2025-07-11 11:24:54 +0900

    😎 [Feature] React Hooks 학습 - useRef

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 // import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Ref from "./Ref";
 // import App from "./App.tsx";
 // import Effect from "./Effect.tsx";
 // import Memo from "./Memo";
-import Average from "./Average";
+// import Average from "./Average";
 
 createRoot(document.getElementById("root")!).render(
     // <StrictMode>
-    <Average />
+    <Ref />
     // </StrictMode>
 );
